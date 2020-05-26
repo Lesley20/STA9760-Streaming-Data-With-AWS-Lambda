cc8feace89bdcc7c74b3eca8bf542c7416aed265
Lesley20 <45520038+Lesley20@users.noreply.github.com>
2020-05-25 20:47:42 -0400

    first commit

---
+++ data_transformer.py
@@ -0,0 +1,13 @@
+import json
+
+def lambda_handler(event, context):
+    output_records = []
+    
+    for record in event["records"]:
+        output_records.append({
+            "recordId": record['recordId'],
+            "result": "Ok",
+            "data": record["data"] + "Cg==" # this is the key here
+        })
+    
+    return { "records": output_records }
